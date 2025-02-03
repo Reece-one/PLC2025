7fbd9a7f7bd7ec4518cfae8b406ff70cb4bad557
Reece N <rnettleford@gmail.com>
2025-02-03 12:13:58 +0000

    task 2 complete

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,6 @@
 main = do
+    let onePlusone = "1 + 1 = 2"
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    putStrLn onePlusone
